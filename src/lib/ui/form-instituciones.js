9bc4d48d8fb33942c3e46564c1c77addc284a8bc
Heidy Gerbi <heidyjoanagr@gmail.com>
2019-06-01 18:34:56 -0500

    agrgando bd a form-instituciones.js

--- src/lib/ui/form-instituciones.js
+++ src/lib/ui/form-instituciones.js
@@ -1,39 +1,42 @@
 export default () => {
-  const listado = 'listado...';
-  const idCheckbox = 'id01';
-  const newArray = [];
-  firebase.firestore().collection('donaciones').onSnapshot((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      newArray.push({
-        id: doc.id,
-        descripcion: doc.data().descripcion
-      })
-    });
-    dataSnapshot(newArray);
-  });   
+ 
   const formElem = document.createElement('form');
   const formContent = `
   <div>
       <h2>Lista de Espera</h2>
       <div>
-        <table border="1">
-          <tr>
-            <td>Lista de Artículos donados</td>
-            <td>Marcar como recibido</td>
-          </tr> 
-          <tr>
-              <td>
-                  ${newArray.descripcion}
-              </td>
-              <td>
-                  <input type="checkbox" id= "${newArray.id}"/>
-              </td>
-          </tr>
+        <table class="container-table" border="1">
+          <thead>
+            <tr>
+              <td>Descripción</td>
+              <td>Cambiar Estatus</td>
+            </tr>
+          </thead>
+          <tbody id="content-list">
+
+          </tbody>
         </table>
       </div>
       <button id="btn-estatus" type="button">Cambiar Estatus</button>
     </div>`;
   formElem.setAttribute('id', 'frm-instituciones');
   formElem.innerHTML = formContent;
+
+  const contentList = document.querySelector('content-list');
+  // console.log(contentList);
+  firebase.firestore().collection('donaciones').onSnapshot((querySnapshot) => {
+    // console.log('entra en query');
+    // console.log(querySnapshot);
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data().descripcion}`);
+      contentList.innerHTML += `
+          <tr css="table">
+              <td> ${doc.data().descripcion} </td>
+              <td><input type="checkbox" id= "${doc.id}"/></td>
+          </tr>`;
+      // console.log('entra aqui');
+    });
+  });
+ 
   return formElem;
 };
