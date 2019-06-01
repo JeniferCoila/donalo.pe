3a3db38e0b0d31fb0d99627fe4fbe6e6c31ab00f
jenifercoila <jeniferfcr@gmail.com>
2019-06-01 15:24:04 -0500

    Mejorando el css de login

--- src/lib/ui/login.js
+++ src/lib/ui/login.js
@@ -1,22 +1,39 @@
 export default () => {
   const formElem = document.createElement('form');
   const formContent = `
-    <form class="d-flex flex-column justify-content-center container-login">
-      <span>DÓNALO.PE</span>
-      <div class="d-flex flex-column justify-content-center" >
-      <label for="">
-        <input placeholder="Usuario" type="text">
-      </label>
-      <label for="">
-        <input placeholder="Contraseña" type="password">
-      </label>
+  <div class="container-login d-flex justify-content-center flex-column">
+    <div class="d-flex justify-content-center margin-bottom margin-top ">
+      <div class="logo">
+      <h1> Dónalo.pe</h1>
+      </div>
+    </div>
+    <div class=" d-flex justify-content-center form-group row">
       <div>
-      <button id= "login-btn" class="btn-width btn btn-lg justify-content-center">
-        INICIA SESION
-      </button>
-    </form>`;
-  formElem.innerHTML = formContent;
+        <label for="inputPassword" class="ancho-label col-form-label"">  <span class="color"></span></label>
+      </div>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" placeholder="Usuario" id="inputPassword" required>
+      </div>
+    </div>
 
+    <div class=" d-flex justify-content-center form-group row">
+      <div>
+        <label for="inputPassword" class="ancho-label col-form-label" "> <span class="color"> 
+        </span></label>
+      </div>
+      <div class="col-sm-9">
+        <input type="password" class="form-control" placeholder="Contraseña" id="inputPassword" required>
+      </div>
+    </div>
+    <div class="d-flex justify-content-center margin-top-low">
+      <button id= "login-btn"class="width btn btn-lg btn-danger justify-content-center">INICIAR SESIÓN</button>
+    </div>
+  </div>
+</div>`;
+  formElem.setAttribute('id', 'frm-login');
+  formElem.setAttribute('class', 'd-flex justify-content-center');
+
+  formElem.innerHTML = formContent;
   const linkForm = formElem.querySelector('#login-btn');
   linkForm.addEventListener('click', () => {
     window.location.hash = '#/formDonante';
