fdb93e136c602216751bcd8bdd287e3f5bec43e3
Betsycampos <cbetsydalia@gmail.com>
2019-06-01 12:03:35 -0500

    Añadiendo botón para incrementar productos que se hagan durante el proceso de donación

--- src/lib/view-controller/frm.js
+++ src/lib/view-controller/frm.js
@@ -1,10 +1,12 @@
 const descripcionValor = document.getElementById('descripcion-input2').value;vcb
 const categoriaSelect = document.getElementById('categoria-select').value;
+const agregaOtros = document.getElementById('agrega-otros');
 const imagenInput = document.getElementById('imagen-input').value;
 const cantidadInput = document.getElementById('cantidad-input').value;
 const institucionInput = document.getElementById('instituciones-select').value;
 const fechaInput = document.getElementById('fecha-input').value;
 const botonDonar = document.getElementById('boton-donar');
+
 
 
 
