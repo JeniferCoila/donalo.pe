2981f312d8c06a38ccffd7d9d0f79f6d0c48daf1
Betsycampos <cbetsydalia@gmail.com>
2019-06-01 11:00:07 -0500

    Añadiendo hojas de formulario a arquitectura

---
+++ src/lib/view-controller/frm.js
@@ -0,0 +1,10 @@
+const descripcionValor = document.getElementById('descripcion-input2').value;vcb
+const categoriaSelect = document.getElementById('categoria-select').value;
+const imagenInput = document.getElementById('imagen-input').value;
+const cantidadInput = document.getElementById('cantidad-input').value;
+const institucionInput = document.getElementById('instituciones-select').value;
+const fechaInput = document.getElementById('fecha-input').value;
+const botonDonar = document.getElementById('boton-donar');
+
+
+
